f04a68611443a140ec9e973f64ab96fc3bfa0af6
Juan Schallmoser <schallmoserjuan@gmail.com>
2025-09-09 10:55:39 -0300

    refactor: :art: add more js and css
    se corrigio y añadio estilos css y codigo javascript

--- js/script.js
+++ js/script.js
@@ -11,3 +11,24 @@ btn.addEventListener("click", () => {
   }
 });
 
+// 
+
+const card = document.getElementById('profileCard');
+const layers = card.querySelectorAll('.card-layer');
+
+card.addEventListener('mousemove', (e) => {
+  const rect = card.getBoundingClientRect();
+  const x = (e.clientX - rect.left) / rect.width - 0.5; // -0.5 a 0.5
+  const y = (e.clientY - rect.top) / rect.height - 0.5;
+
+  layers.forEach((layer, i) => {
+    const depth = (i + 1) * 5; // cuanto mayor el índice, más se mueve
+    layer.style.transform = `translateX(${x * depth}px) translateY(${y * depth}px)`;
+  });
+});
+
+card.addEventListener('mouseleave', () => {
+  layers.forEach(layer => {
+    layer.style.transform = 'translateX(0px) translateY(0px)';
+  });
+});
